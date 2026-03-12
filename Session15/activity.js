15775c5bcbf878b58e695a71e4075c79f01a75a2
Clarira03 <fcimanlutac.student@ua.edu.ph>
2026-03-12 19:45:16 +0800

    Activity done

---
+++ Session15/activity.js
@@ -0,0 +1,10 @@
+// Celsius to kelvin
+let celsius = prompt("Enter Celcius: ")
+let kelvin = celsius * 1 + 273.15;
+alert ("temperature in kelvin: " + kelvin);
+
+// Circle Area
+let radius = prompt ("Enter the radius of the circle: ");
+let num1 = 3.14;
+let area = num1 * radius * radius;
+alert("Area of the circle:" + area);
