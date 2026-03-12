a7d7072cce2fca0f39a55244c1130e9635a1e8df
Clarira03 <fcimanlutac.student@ua.edu.ph>
2026-03-12 11:53:52 +0800

    Ssion 14

---
+++ Session14/index.js
@@ -0,0 +1,54 @@
+//this is how to print an output
+// f12 pre
+// console.log("content");
+console.log("hello world");
+
+// VARIABLES (stores data)
+// DATA TYPES 
+//1. Numeric DT
+/*
+let variableName; -> no initilazed value
+let variableNAme - value -> wd initialization
+
+ */
+let age; 
+let height=160;
+console.log(height);
+
+// JS is a loose type  PL ket walang colon or may space(;)
+console.log(height)
+console.    log     ( height  )
+//sychronous PL kasi siya 
+let gwa=99.75;
+console.log(gwa);
+let firstName = "John"
+console.log(firstName);
+
+// typeof -> to check a variable's type of data
+console.log(typeof firstName);
+console.log(typeof gwa);
+console.log(typeof height);
+
+//Text casing 
+//Camel -> thisIsCamelCasing
+//snake -> this_is_snake
+//Pascal-> ThisisPascal
+//Kebab -> this-is-kebabl
+//Pascal Kebab -> Tis-Is- Wah
+
+//Boolean Dat types
+let isMarried = false;
+let isRegistered = true;
+let hasExpired = true;
+
+console.log(isMarried);
+console.log(isRegistered);
+console.log(typeof isRegistered);
+
+//Concatenation
+let province = "Pampanga";
+console.log("I live in " + province)
+
+// Type- Coercion
+// Concatenation of a string and number or other data types will be all converted to string
+console.log("My overall grade is " + gwa)
