1df7111cfdac380a71575fa504f88cd286206a0d
Clarira03 <fcimanlutac.student@ua.edu.ph>
2026-03-26 11:53:17 +0800

    Added Session 18

---
+++ Session18/activity.js
@@ -0,0 +1,30 @@
+function startCalc(){
+    let userInput = parseInt(prompt("Please select an action: \n [1] Input Numbers \n [2] Sum\n [3] Difference \n [4] Product \n [5]Qoutient\n [6]Reset" ));
+    switch (userInput){
+function enterNumbers(){
+    let inputNum1 = parseInt(prompt("Enter first number."));
+    let inputNum2 = parseInt(prompt("Enter second number."));
+    num1 = inputNum1;
+    num2 = inputNum2;
+}
+
+function sum(){
+    if(num1 == 0 || num2 == 0){
+        alert("Please enter num1 or num2.")
+    }else{
+        let sum = num1 + num2;
+        alert(`The Sum of ${num1} and ${num2} is ${sum}.`);
+        
+    } else if {
+        let diff = num1 - num2;
+        alert(`The Difference of ${num1} and ${num2} is ${diff}.`);
+    } else if {
+        let quotient = num1 / num2;
+        alert(`The Quotient of ${num1} and ${num2} is ${quotient}.`);
+    } else if {
+        let 
+    } else if {
+
+    }
+        
+    }
---
+++ Session18/index.js
@@ -0,0 +1,48 @@
+//console.log("")
+
+
+//Section JS Functions
+//Performs specific tasks, eliminate repetetive class
+
+//  SYNTAX
+//function functionName(parameter){//code black}
+
+console.log("Hello World!")
+
+function sayHello(){
+    console.log("Hello, City of San Fernando, Pampanga!");
+}
+//Invocation/Invoke of Function Calling
+sayHello();
+sayHello();
+sayHello();
+sayHello();
+sayHello();
+
+function showAlert(){
+    alert("Hello, User!");
+}
+
+function startCalc(){
+    let userInput = parseInt(prompt("Please select an action: \n [1] Input Numbers \n [2] Sum\n [3] Difference \n [4] Product \n [5]Qoutient\n [6]Reset" ));
+    switch (userInput){
+        case 1;
+        userInput();
+        break;
+    }
+        case 2:
+        sum();
+        break;
+    }
+
+
+function enterNumbers(){
+    let inputNum1 = parseInt(prompt("Enter the First Number"));
+    let inputNum2 = parseInt(prompt("Enter the Second Number"));
+    num1=inputNum1
+    num2=inputNum2
+}
+
+function sum(){
+    alert("The Sum of  and ")
+}
